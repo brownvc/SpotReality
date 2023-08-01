e14a0b106605cd04f24ea2b2588b0aae45490c0d
Are-Oelsner <are_oelsner@brown.edu>
2023-08-01 14:52:18 -0400

    feat: fixed intrinsics, added T slider to go from point cloud to flat image. Automne & Are

---
+++ Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/ImageSubscribers.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageSubscribers : MonoBehaviour
+{
+ // public JPEGImageSubscriber frontleftImage;
+ // public JPEGImageSubscriber frontrightImage;
+ // public JPEGImageSubscriber leftImage;
+ // public JPEGImageSubscriber rightImage;
+ // public JPEGImageSubscriber backImage;
+ // public JPEGImageSubscriber handImage;
+
+ // public RAWImageSubscriber frontrightDepth;
+ // public RAWImageSubscriber frontleftDepth;
+ // public RAWImageSubscriber leftDepth;
+ // public RAWImageSubscriber rightDepth;
+ // public RAWImageSubscriber backDepth;
+ // public RAWImageSubscriber handDepth;
+ // public string[] topics = { "/spot/stream_image/frontleft_fisheye_image/image", "/spot/stream_image/frontright_fisheye_image/image" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // frontleftImage = new JPEGImageSubscriber();
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
