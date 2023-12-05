3bf933a41a30a4807f9f9310e53d075a7d4433d5
VR Teleop <nivlac12@gmail.com>
2023-12-05 13:03:05 -0500

    Fixed movement relative to angle

--- Assets/HeightAdjuster.cs
+++ Assets/HeightAdjuster.cs
@@ -25,31 +25,49 @@ public class HeightAdjuster : MonoBehaviour
     {
         Vector2 leftMove;
         Vector2 rightMove;
+        Quaternion relativeRot;
+        bool low;
+        bool high;
+
 
         /* Set the camera higher or lower */
-        // Detect input values
-        bool low = goLower.action.IsPressed();
-        bool high = goHigher.action.IsPressed();
+        low = goLower.action.IsPressed();
+        high = goHigher.action.IsPressed();
 
-
-        // Go lower
+        /* Go lower */
         if (low && !high)
         {
             cameraTransform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y - speed, cameraTransform.position.z);
         }
-        // Go higher
+        /* Go higher */
         else if(!low && high)
         {
             cameraTransform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y + speed, cameraTransform.position.z);
         }
 
-        /* Move camera position around */
+        /* Move camera position around according to left stick */
         leftMove = LAx.action.ReadValue<Vector2>() / 50f;
-        cameraTransform.position += cameraTransform.rotation * new Vector3(leftMove.x, 0f, leftMove.y);
+        relativeRot = Quaternion.Euler(0f, cameraTransform.rotation.eulerAngles.y, 0f);// cameraTransform.rotation;
+        cameraTransform.position += relativeRot * new Vector3(leftMove.x, 0f, leftMove.y);
 
-        /* Move camera rotation */
+        /* Adjust camera rotation according to right stick*/
         rightMove = RAx.action.ReadValue<Vector2>();
-        cameraTransform.Rotate(new Vector3(0f, rightMove.x, 0f));
-
+        if (rightMove.magnitude > 0f)
+        {
+            /* Only change one axis at a time */
+            if (Math.Abs(rightMove.x) > Math.Abs(rightMove.y))
+            {
+                /* Rotate left/right relative to world space */
+                cameraTransform.Rotate(new Vector3(0f, rightMove.x, 0f), Space.World);
+            }
+            else
+            {
+                /* Rotate up/down relative to world space */
+                /* Disabled for now */
+                // cameraTransform.Rotate(new Vector3(rightMove.y * 0.5f, 0f, 0f), Space.World);
+            }
+            /* Don't allow z rotation to change */
+            cameraTransform.rotation = Quaternion.Euler(new Vector3(cameraTransform.rotation.eulerAngles.x, cameraTransform.rotation.eulerAngles.y, 0f));
+        }
     }
 }
