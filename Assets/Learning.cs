33bf8d191cd942cdafac2635e37cf78a01f0ae68
Calvin Bauer <nivlac12@gmail.com>
2024-04-11 14:03:04 -0400

    Added Barracuda for matrix operations

--- Assets/Learning.cs
+++ Assets/Learning.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using Unity.Barracuda;
 
 public class Learning : MonoBehaviour
 {
@@ -27,10 +28,11 @@ public class Learning : MonoBehaviour
 
 public class SoftActorCritic
 {
-    Vector3 originalPos;
-    Quaternion originalRot;
-    Transform agentTransform;
-    Transform goalTransform;
+    private Vector3 originalPos;
+    private Quaternion originalRot;
+    private Transform agentTransform;
+    private Transform goalTransform;
+    private const float TERMINALDIST = 0.5f;
 
     public SoftActorCritic(Transform aT, Transform gT)
     {
@@ -59,22 +61,23 @@ public class SoftActorCritic
     {
         Vector3 pos = agentTransform.position;
         Quaternion rot = agentTransform.rotation;
+        float term = terminal() ? 1f : 0f;
 
-        // Return the positions
-        return new float[] { pos.x, pos.y, pos.z, rot.x, rot.y, rot.z, rot.w };
+        // Return the positions, plus whether terminal
+        return new float[] { pos.x, pos.y, pos.z, rot.x, rot.y, rot.z, rot.w, term };
 
-        // TODO: add whether in a place to pick up the distance
+        // TODO: add whether in a place to pick up the goal object
     }
 
-    private double getReward()
+    private double reward()
     {
         // Return the inverse of the distance between the objects
         float distance = Vector3.Distance(agentTransform.position, goalTransform.position);
 
         // Account for getting very close
-        if (distance < 0.5)
+        if (distance < TERMINALDIST)
         {
-            return 2;
+            return 1 / TERMINALDIST;
         }
         else
         {
@@ -87,13 +90,19 @@ public class SoftActorCritic
 
     private bool terminal()
     {
-        return Vector3.Distance(agentTransform.position, goalTransform.position) < 0.5;
+        return Vector3.Distance(agentTransform.position, goalTransform.position) < TERMINALDIST;
     }
 
     // Softmax policy
     private float[] softmaxPi(float[] theta, float[] stateFeats)
     {
-        //Get numsharp for matrix operations!
+        // Create the policy tensor
+        TensorShape thetaShape = new TensorShape(3);
+        Tensor pi = new Tensor(thetaShape);
+
+        // Get the denominator
+
+
 
         return new float[] { 1 };
     }
