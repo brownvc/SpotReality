bd055aa50b737c5c2499852bc26f820a1482693a
VR Teleop <nivlac12@gmail.com>
2024-09-07 13:01:13 -0400

    add support for multispot

--- Assets/DepthManager.cs
+++ Assets/DepthManager.cs
@@ -12,27 +12,43 @@ public class DepthManager : MonoBehaviour
 
     public float edge_threshold;
 
-    private float[] depth_left;
-    private Texture2D rgb_left;
 
-    private float[] depth_right;
-    private Texture2D rgb_right;
 
-    private float[] output_left = new float[480 * 640];
-    private float[] output_right = new float[480 * 640];
+    private float[] spot1_depth_left;
+    private Texture2D spot1_rgb_left;
 
-    private bool received_left = false;
-    private bool received_right = false;
+    private float[] spot1_depth_right;
+    private Texture2D spot1_rgb_right;
+
+    private float[] spot2_depth_left;
+    private Texture2D spot2_rgb_left;
+
+    private float[] spot2_depth_right;
+    private Texture2D spot2_rgb_right;
+
+
+
+    private float[] spot1_output_left = new float[480 * 640];
+    private float[] spot1_output_right = new float[480 * 640];
+    private float[] spot2_output_left = new float[480 * 640];
+    private float[] spot2_output_right = new float[480 * 640];
+
+    private bool spot1_received_left = false;
+    private bool spot1_received_right = false;
+    private bool spot2_received_left = false;
+    private bool spot2_received_right = false;
 
     private bool depth_process_lock = false;
 
-    public DepthAveraging AveragerLeft;
-    public DepthAveraging AveragerRight;
+    public DepthAveraging Spot1AveragerLeft;
+    public DepthAveraging Spot1AveragerRight;
+    public DepthAveraging Spot2AveragerLeft;
+    public DepthAveraging Spot2AveragerRight;
 
-    public DrawMeshInstanced Left_Depth_Renderer;
-    public DrawMeshInstanced Right_Depth_Renderer;
-
-    bool first_run = false;
+    public DrawMeshInstanced Spot1_Left_Depth_Renderer;
+    public DrawMeshInstanced Spot1_Right_Depth_Renderer;
+    public DrawMeshInstanced Spot2_Left_Depth_Renderer;
+    public DrawMeshInstanced Spot2_Right_Depth_Renderer;
 
     private float deltaTime = 0.0f;
 
@@ -48,11 +64,10 @@ public class DepthManager : MonoBehaviour
         
     }
 
-
     private IEnumerator ResetActivateDepthEstimation()
     {
         activate_depth_estimation = false;
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.5f);
         activate_depth_estimation = true;
     }
 
@@ -66,77 +81,122 @@ public class DepthManager : MonoBehaviour
 
     public float[] update_depth_from_renderer(Texture2D rgb, float[] depth, int camera_index)
     {
-        if (camera_index == 0 && !received_left)
+        if (camera_index == 0 && !spot1_received_left)
         {
-            depth_left = (float[])depth.Clone();
+            spot1_depth_left = (float[])depth.Clone();
 
-            if (rgb_left != null)
+            if (spot1_rgb_left != null)
             {
-                Destroy(rgb_left);
+                Destroy(spot1_rgb_left);
             }
-            rgb_left = new Texture2D(rgb.width, rgb.height, rgb.format, rgb.mipmapCount > 1);
-            Graphics.CopyTexture(rgb, rgb_left);
-            received_left = true;
+            spot1_rgb_left = new Texture2D(rgb.width, rgb.height, rgb.format, rgb.mipmapCount > 1);
+            Graphics.CopyTexture(rgb, spot1_rgb_left);
+            spot1_received_left = true;
         }
-        else if (camera_index == 1 && !received_right)
+        else if (camera_index == 1 && !spot1_received_right)
         {
-            depth_right = (float[])depth.Clone();
+            spot1_depth_right = (float[])depth.Clone();
 
-            if (rgb_right != null)
+            if (spot1_rgb_right != null)
             {
-                Destroy(rgb_right);
+                Destroy(spot1_rgb_right);
             }
-            rgb_right = new Texture2D(rgb.width, rgb.height, rgb.format, rgb.mipmapCount > 1);
-            Graphics.CopyTexture(rgb, rgb_right);
-            received_right = true;
+            spot1_rgb_right = new Texture2D(rgb.width, rgb.height, rgb.format, rgb.mipmapCount > 1);
+            Graphics.CopyTexture(rgb, spot1_rgb_right);
+            spot1_received_right = true;
+        }
+        else if (camera_index == 2 && !spot2_received_left)
+        {
+            spot2_depth_left = (float[])depth.Clone();
+
+            if (spot2_rgb_left != null)
+            {
+                Destroy(spot2_rgb_left);
+            }
+            spot2_rgb_left = new Texture2D(rgb.width, rgb.height, rgb.format, rgb.mipmapCount > 1);
+            Graphics.CopyTexture(rgb, spot2_rgb_left);
+            spot2_received_left = true;
+        }
+        else if (camera_index == 3 && !spot2_received_right)
+        {
+            spot2_depth_right = (float[])depth.Clone();
+
+            if (spot2_rgb_right != null)
+            {
+                Destroy(spot2_rgb_right);
+            }
+            spot2_rgb_right = new Texture2D(rgb.width, rgb.height, rgb.format, rgb.mipmapCount > 1);
+            Graphics.CopyTexture(rgb, spot2_rgb_right);
+            spot2_received_right = true;
         }
 
-        if (received_left && received_right && !depth_process_lock)
+
+        if (spot1_received_left && spot1_received_right && spot2_received_left && spot2_received_right && !depth_process_lock)
         {
             depth_process_lock = true;
 
-            bool not_moving = Left_Depth_Renderer.get_ready_to_freeze() && Right_Depth_Renderer.get_ready_to_freeze();
-            //not_moving = true;
-            (output_left, output_right) = process_depth(depth_left, rgb_left, depth_right, rgb_right, not_moving);
+            bool not_moving = Spot1_Left_Depth_Renderer.get_ready_to_freeze() && Spot1_Right_Depth_Renderer.get_ready_to_freeze()
+                && Spot2_Left_Depth_Renderer.get_ready_to_freeze() && Spot2_Right_Depth_Renderer.get_ready_to_freeze();
 
-            received_left = false;
-            received_right = false;
+            //not_moving = true;
+            (spot1_output_left, spot1_output_right, spot2_output_left, spot2_output_right) = process_depth(spot1_depth_left, spot1_rgb_left, spot1_depth_right, spot1_rgb_right,
+                spot2_depth_left, spot2_rgb_left, spot2_depth_right, spot2_rgb_right, not_moving);
+
+            spot1_received_left = false;
+            spot1_received_right = false;
+            spot2_received_left = false;
+            spot2_received_right = false;
 
             depth_process_lock = false;
-            first_run = true;
         }
 
         if (camera_index == 0)
         {
-            return output_left;
+            return spot1_output_left;
         }
         else if (camera_index == 1)
         {
-            return output_right;
+            return spot1_output_right;
+        }
+        else if (camera_index == 2)
+        {
+            return spot2_output_left;
+        }
+        else if (camera_index == 3)
+        {
+            return spot2_output_right;
         }
 
-        return output_right;
+        return spot1_output_left;
     }
 
-    private (float[], float[]) process_depth(float[] depthL, Texture2D rgbL, float[] depthR, Texture2D rgbR, bool is_not_moving)
+    private (float[], float[], float[], float[]) process_depth(float[] depthL1, Texture2D rgbL1, 
+                                            float[] depthR1, Texture2D rgbR1,
+                                            float[] depthL2, Texture2D rgbL2,
+                                            float[] depthR2, Texture2D rgbR2, 
+                                            bool is_not_moving)
     {
         if (median_averaging && mean_averaging)
         {
             mean_averaging = false;
         }
 
-        float[] temp_output_left = depthL, temp_output_right = depthR;
+        float[] spot1_temp_output_left = depthL1, spot1_temp_output_right = depthR1;
+        float[] spot2_temp_output_left = depthL2, spot2_temp_output_right = depthR2;
 
         // depth completion
-        //Debug.Log("depth completion");
         if (activate_depth_estimation && is_not_moving)
         {
-            (temp_output_left, temp_output_right) = GetComponent<DepthCompletion>().complete(depthL, rgbL, depthR, rgbR);
+            (spot1_temp_output_left, spot1_temp_output_right, spot2_temp_output_left, spot2_temp_output_right) = 
+                GetComponent<DepthCompletion>().complete(depthL1, rgbL1, depthR1, rgbR1,
+                                                        depthL2, rgbL2, depthR2, rgbR2);
         }
 
-        temp_output_left = AveragerLeft.averaging(temp_output_left, is_not_moving, mean_averaging, median_averaging, edge_detection, edge_threshold);
-        temp_output_right = AveragerRight.averaging(temp_output_right, is_not_moving, mean_averaging, median_averaging, edge_detection, edge_threshold);
+        spot1_temp_output_left = Spot1AveragerLeft.averaging(spot1_temp_output_left, is_not_moving, mean_averaging, median_averaging, edge_detection, edge_threshold);
+        spot1_temp_output_right = Spot1AveragerRight.averaging(spot1_temp_output_right, is_not_moving, mean_averaging, median_averaging, edge_detection, edge_threshold);
+        spot2_temp_output_left = Spot2AveragerLeft.averaging(spot2_temp_output_left, is_not_moving, mean_averaging, median_averaging, edge_detection, edge_threshold);
+        spot2_temp_output_right = Spot2AveragerRight.averaging(spot2_temp_output_right, is_not_moving, mean_averaging, median_averaging, edge_detection, edge_threshold);
 
-        return (temp_output_left, temp_output_right);
+        return (spot1_temp_output_left, spot1_temp_output_right, spot2_temp_output_left, spot2_temp_output_right);
     }
 }
--- Assets/Scripts/DepthCompletion.cs
+++ Assets/Scripts/DepthCompletion.cs
@@ -34,7 +34,10 @@ public class DepthCompletion : MonoBehaviour
     // =============================================================================== //
     //                               Depth Completion                                  //
     // =============================================================================== //
-    public (float[], float[]) complete(float[] depth_data_0, Texture2D color_data_0, float[] depth_data_1, Texture2D color_data_1)
+    public (float[], float[], float[], float[]) complete(float[] depth_data_0, Texture2D color_data_0, 
+        float[] depth_data_1, Texture2D color_data_1, 
+        float[] depth_data_2, Texture2D color_data_2,
+        float[] depth_data_3, Texture2D color_data_3)
     {
         TensorShape depth_shape = new TensorShape(1, 1, 480, 640);
         TensorShape color_shape = new TensorShape(1, 3, 480, 640);
@@ -47,12 +50,24 @@ public class DepthCompletion : MonoBehaviour
         using TensorFloat color_tensor_1 = TextureConverter.ToTensor(color_data_1, channels: 3);
         color_tensor_1.Reshape(color_shape);
 
+        using TensorFloat depth_tensor_2 = new TensorFloat(depth_shape, depth_data_2);
+        using TensorFloat color_tensor_2 = TextureConverter.ToTensor(color_data_2, channels: 3);
+        color_tensor_2.Reshape(color_shape);
+
+        using TensorFloat depth_tensor_3 = new TensorFloat(depth_shape, depth_data_3);
+        using TensorFloat color_tensor_3 = TextureConverter.ToTensor(color_data_3, channels: 3);
+        color_tensor_3.Reshape(color_shape);
+
         Dictionary<string, Tensor> input_tensors = new Dictionary<string, Tensor>()
         {
             { "rgb_0", color_tensor_0 },
             { "depth_0", depth_tensor_0 },
             { "rgb_1", color_tensor_1 },
             { "depth_1", depth_tensor_1 },
+            { "rgb_2", color_tensor_2 },
+            { "depth_2", depth_tensor_2 },
+            { "rgb_3", color_tensor_3 },
+            { "depth_3", depth_tensor_3 },
         };
 
         worker.Execute(input_tensors);
@@ -65,11 +80,19 @@ public class DepthCompletion : MonoBehaviour
         depth_outputTensor_1.CompleteOperationsAndDownload();
         float[] output_depth_1 = depth_outputTensor_1.ToReadOnlyArray();
 
+        using TensorFloat depth_outputTensor_2 = worker.PeekOutput("output_depth_2") as TensorFloat;
+        depth_outputTensor_2.CompleteOperationsAndDownload();
+        float[] output_depth_2 = depth_outputTensor_2.ToReadOnlyArray();
+
+        using TensorFloat depth_outputTensor_3 = worker.PeekOutput("output_depth_3") as TensorFloat;
+        depth_outputTensor_3.CompleteOperationsAndDownload();
+        float[] output_depth_3 = depth_outputTensor_3.ToReadOnlyArray();
+
         foreach (var tensor in input_tensors.Values)
         {
             tensor.Dispose();
         }
 
-        return (output_depth_0, output_depth_1);
+        return (output_depth_0, output_depth_1, output_depth_2, output_depth_3);
     }
 }
