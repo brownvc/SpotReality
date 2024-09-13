8163b440364ed338b9053231eddf78be02fb5668
lllllcf <chengfan_li@brown.edu>
2024-09-13 13:56:47 -0400

    temp

--- Assets/DepthManager.cs
+++ Assets/DepthManager.cs
@@ -74,7 +74,6 @@ public class DepthManager : MonoBehaviour
     {
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
         float fps = 1.0f / deltaTime;
-        fpsText.text = Mathf.Ceil(fps).ToString();
     }
 
     public float[] update_depth_from_renderer(Texture2D rgb, float[] depth, int camera_index)
