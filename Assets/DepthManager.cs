2eee15999a55e9935f402c49ef3706abe19bc10d
lllllcf <chengfan_li@brown.edu>
2024-09-16 16:34:29 -0400

    remove fps text

--- Assets/DepthManager.cs
+++ Assets/DepthManager.cs
@@ -36,8 +36,6 @@ public class DepthManager : MonoBehaviour
 
     private float deltaTime = 0.0f;
 
-    public Text fpsText;
-
     // Start is called before the first frame update
     void Start()
     {
